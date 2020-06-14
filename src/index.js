02172a496334ea2a939b19b761cefe14939ae555
JasxonWang <i@iiong.com>
2020-06-14 11:36:47 +0800

    🕟 更新业务

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const url = require('url')
 const path = require('path')
 const BaseAdapter = require('ghost-storage-base')
 const qiniu = require('qiniu')
-const moment = require('moment')
+const dayjs = require('dayjs')
 const fetch = require('node-fetch')
 
 
@@ -21,19 +21,19 @@ const compile = source => {
 }
 
 /**
- * Get path format context
+ * 获取上传的文件基本对象
+ * @param file
+ * @returns file object
  */
-const getPathContext = original => {
-  const date = moment()
+
+const getPathContext = file => {
   return {
-    year: date.format('YYYY'),
-    month: date.format('MM'),
-    day: date.format('DD'),
-    hour: date.format('HH'),
-    timestamp: date.format('x'),
-    random: Math.random().toString().substr(-8),
-    ext: path.extname(original),
-    name: path.basename(original, path.extname(original))
+    year: dayjs().format('YYYY'),
+    month: dayjs().format('MM'),
+    day: dayjs().format('DD'),
+    hour: dayjs().format('HH'),
+    ext: path.extname(file),
+    name: path.basename(file, path.extname(file))
   }
 }
 
@@ -56,30 +56,32 @@ class GhostQiNiuStoreAdapter extends BaseAdapter {
 
     this.dirFormat = path.dirname(format)
     this.nameFormat = path.basename(format)
+
+    const mac = new qiniu.auth.digest.Mac(this.accessKey, this.secretKey)
+    const putPolicy = new qiniu.rs.PutPolicy({
+      scope: bucket
+    })
+    this.qnUploadToken = putPolicy.uploadToken(mac)
+    // todo: 构建配置类
+    this.qnConfig = new qiniu.conf.Config()
+    // 对象上传类
+    this.qnUploader = new qiniu.form_up.FormUploader(this.qnConfig)
+    // 对象管理类
+    this.qnBucketManager = new qiniu.rs.BucketManager(mac, this.qnConfig)
   }
 
-  exists(file, targetDir) {
+  exists(fileName, targetDir) {
     return new Promise((resolve, reject) => {
       resolve(false)
     })
   }
 
   save(file, targetDir) {
-    const context = getPathContext(file.name)
+    const context = getPathContext(file.name) // 获取上传文件基本对象
 
     const upload = key => new Promise((resolve, reject) => {
-      const mac = new qiniu.auth.digest.Mac(this.accessKey, this.secretKey)
-      const putPolicy = new qiniu.rs.PutPolicy({
-        scope: this.bucket
-      })
-      const uploadToken = putPolicy.uploadToken(mac)
-      // 构建配置类
-      const config = new qiniu.conf.Config()
-      const uploader = new qiniu.form_up.FormUploader(config)
-
       const putExtra = new qiniu.form_up.PutExtra()
-
-      uploader.putFile(uploadToken, key, file.path, putExtra, (responseError, responseBody, responseInfo) => {
+      this.qnUploader.putFile(this.qnUploadToken, key, file.path, putExtra, (responseError, responseBody, responseInfo) => {
         if (responseError) return reject(responseError)
         if (responseInfo.statusCode !== 200) return reject(new Error(responseBody.error))
         resolve(responseBody)
@@ -99,15 +101,29 @@ class GhostQiNiuStoreAdapter extends BaseAdapter {
     return (req, res, next) => next()
   }
 
-  delete() {
-    return Promise.reject(new Error('Not implemented'))
+  delete(file) {
+    const pathname = url.parse(file.path).pathname
+    if (!pathname) return Promise.reject(new Error(`Could not read file: ${file.path}`))
+    return new Promise((resolve, reject) => {
+      this.qnBucketManager.delete(this.bucket, pathname, (responseError, responseBody, responseInfo) => {
+        if (responseError) reject(responseError)
+        if (responseInfo.statusCode !== 200) reject(new Error(responseBody.error))
+        resolve(responseBody)
+      })
+    })
   }
 
   read(file) {
     const pathname = url.parse(file.path).pathname
     if (!pathname) return Promise.reject(new Error(`Could not read file: ${file.path}`))
-    return fetch(url.resolve(this.domain, pathname)).then(response => response.buffer())
+    return new Promise((resolve, reject) => {
+      fetch(url.resolve(this.domain, pathname)).then(response => {
+        resolve(response.buffer())
+      }).catch(error => {
+        reject(error)
+      })
+    })
   }
 }
 
-module.exports = GhostQiNiuStoreAdapter;
+module.exports = GhostQiNiuStoreAdapter
